a8c4a79706a81517267ea7baab95e2e94ce9b293
Tiait1999 <aitzianethinhinane@gmail.com>
2026-06-25 15:19:23 +0200

    CommitAddroduit

--- demo/src/test/java/com/example/RegisterPomTest.java
+++ demo/src/test/java/com/example/RegisterPomTest.java
@@ -3,15 +3,23 @@ package com.example;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.example.pom.MainPomPage;
 import com.example.pom.Register1;
 import com.example.pom.RegisterPom;
+import com.example.pom.loginpage;
+import com.example.pom.cartpom;
+
 
 public class RegisterPomTest {
 
@@ -19,6 +27,8 @@ public class RegisterPomTest {
     MainPomPage mp;
     RegisterPom rp;
     Register1 reg;
+    loginpage lp;
+    cartpom cp;
     String mail = "user"+Math.floor(Math.random()*5000)+"@gmail.com";
     String pass = "user"+(Math.floor(Math.random()*5000)+1000);
 
@@ -30,6 +40,9 @@ public class RegisterPomTest {
         mp = new MainPomPage(driver);
         rp = new RegisterPom(driver);
         reg = new Register1(driver);
+        lp = new loginpage(driver);
+        cp = new cartpom(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://demowebshop.tricentis.com/");
     }
 
@@ -57,7 +70,30 @@ public class RegisterPomTest {
         assertEquals("Your registration completed", reg.Message()); 
         //reg.ContinuerBtn();
         reg.ContinuerBtn2();  
-        assertEquals("https://demowebshop.tricentis.com/", driver.getCurrentUrl());     
+        assertEquals("https://demowebshop.tricentis.com/", driver.getCurrentUrl()); 
+        // se deconnecter
+        mp.clicklogout();
+        // se connecter
+        mp.bouttonlogin(); 
+        lp.AppLogin(mail, pass);
+        assertEquals("https://demowebshop.tricentis.com/", driver.getCurrentUrl());
+        mp.clickaddtocard();
+        //verifier que le produit est ajouter avec la bare du message
+        //WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        //String msg = wait.until(ExpectedConditions.visibilityOfElementLocated(mp.MsgAddPro)).getText();
+        //assertEquals(msg,"The product has been added to your shopping cart");
+        assertEquals(mp.MessageAddPro(),"The product has been added to your shopping cart");
+        // clique vers url vers shoping cart
+        mp.clickGoToCart();
+        // verifier qu on est rederiger vers la page cart
+        assertEquals("https://demowebshop.tricentis.com/cart", driver.getCurrentUrl());
+        //verifier le produit dans cart
+        cp.ClickIagree();
+        //clique checkout
+        cp.ClickCheckOut();
+        // verifier la redirection vers la page checkout
+        assertEquals("https://demowebshop.tricentis.com/onepagecheckout", driver.getCurrentUrl());
+
     }
 
 
---
+++ demo/src/test/java/com/example/pom/Webdriver.java
@@ -0,0 +1,12 @@
+package com.example.pom;
+
+import org.openqa.selenium.By;
+
+public class Webdriver {
+
+    public Object findElement(By email) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findElement'");
+    }
+
+}
